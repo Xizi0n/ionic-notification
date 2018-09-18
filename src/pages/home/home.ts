5ce6bcf43ab2f9abd4e789039823b96d7558d09d
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2018-09-18 15:44:20 +0200

    base

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications,
+              private platform: Platform, public alertCtrl: AlertController) {
 
+  
+  }
+
+  btnPushClicked() {
+    this.platform.ready().then( () => {
+      this.localNotifications.schedule({
+        text: 'Delayed ILocalNotification',
+        trigger: {at: new Date(new Date().getTime() + 3600)},
+        led: 'FF0000',
+        sound: null
+      })
+    });
   }
 
 }
