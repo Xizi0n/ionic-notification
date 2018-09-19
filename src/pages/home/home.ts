46f1c1b9133d50fffb1290133b4ec5627c6dde7b
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2018-09-19 12:05:42 +0200

    lockscreen notification+foregroud noti

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,10 +18,36 @@ export class HomePage {
   btnPushClicked() {
     this.platform.ready().then( () => {
       this.localNotifications.schedule({
-        text: 'Delayed ILocalNotification',
+        id: 2,
+        title: 'Incoming Call Bitch',
+        text: 'Get notified pls',
+        icon: 'http://example.com/icon.png',
+        actions : [
+          { id: 'accept', title: 'Felvesz' },
+          { id: 'reject', title: 'Lerak' }
+        ],
+        foreground: true,
+        lockscreen: true,
+        color: '00FF00'
+      })
+    });
+  }
+
+  secondButtonClick() {
+    this.platform.ready().then( () => {
+      this.localNotifications.schedule({
+        id: 2,
+        title: 'Incoming Call Bitch',
+        text: 'Get notified pls',
+        icon: 'http://example.com/icon.png',
+        actions : [
+          { id: 'accept', title: 'Felvesz' },
+          { id: 'reject', title: 'Lerak' }
+        ],
+        foreground: true,
+        lockscreen: true,
         trigger: {at: new Date(new Date().getTime() + 3600)},
-        led: 'FF0000',
-        sound: null
+        color: '00FF00'
       })
     });
   }
